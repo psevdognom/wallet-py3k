05215bac294ab3b57e6c8fe4530bd1d6bfbd328f
Egor Orlov <egor.orlov@ostrovok.ru>
2015-10-16 01:05:50 +0300

    updated setup.py for python3

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Other Environment',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
